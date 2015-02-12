3b55842b9158d2ea1004b3246b3a8756158b1900
Max Ren <sonicfire3000@hotmail.com>
2015-02-12 10:36:26 -0500

    Added spectators array to game controller. Names are random, use 'npm install chance' to acquire node dependency for Chance.js

--- api/controllers/GameController.js
+++ api/controllers/GameController.js
@@ -5,10 +5,17 @@
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
 
+var Chance = require('chance');
+var chance = new Chance();
+var name = chance.name();
+
+var spectators = [chance.name(), chance.name(), chance.name(), chance.name()];
+
 module.exports = {
   home: function(req, res) {
     return res.view({
-      todo: 'Not implemented yet!',
+      //todo: 'Not implemented yet!',
+      specs: spectators,
     });
   },
 };
