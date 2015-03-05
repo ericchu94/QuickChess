33e9a4485a63832853c105d6e51a32a92947eb78
Eric Chu <eric@ericchu.net>
2015-03-05 16:28:39 -0500

    Moved game state to controller

--- api/controllers/GameController.js
+++ api/controllers/GameController.js
@@ -8,6 +8,52 @@
 var Chance = require('chance');
 var chance = new Chance();
 var name = chance.name();
+var defaultBoard = [
+  [
+    { color: "white", piece: "r" },
+    { color: "white", piece: "n" },
+    { color: "white", piece: "b" },
+    { color: "white", piece: "q" },
+    { color: "white", piece: "k" },
+    { color: "white", piece: "b" },
+    { color: "white", piece: "n" },
+    { color: "white", piece: "r" },
+  ],
+  [
+    { color: "white", piece: "p" },
+    { color: "white", piece: "p" },
+    { color: "white", piece: "p" },
+    { color: "white", piece: "p" },
+    { color: "white", piece: "p" },
+    { color: "white", piece: "p" },
+    { color: "white", piece: "p" },
+    { color: "white", piece: "p" },
+  ],
+  [ null, null, null, null, null, null, null, null, null ],
+  [ null, null, null, null, null, null, null, null, null ],
+  [ null, null, null, null, null, null, null, null, null ],
+  [ null, null, null, null, null, null, null, null, null ],
+  [
+    { color: "black", piece: "p" },
+    { color: "black", piece: "p" },
+    { color: "black", piece: "p" },
+    { color: "black", piece: "p" },
+    { color: "black", piece: "p" },
+    { color: "black", piece: "p" },
+    { color: "black", piece: "p" },
+    { color: "black", piece: "p" },
+  ],
+  [
+    { color: "black", piece: "r" },
+    { color: "black", piece: "n" },
+    { color: "black", piece: "b" },
+    { color: "black", piece: "q" },
+    { color: "black", piece: "k" },
+    { color: "black", piece: "b" },
+    { color: "black", piece: "n" },
+    { color: "black", piece: "r" },
+  ],
+];
 
 var spectators = [chance.name(), chance.name(), chance.name(), chance.name()];
 
@@ -16,6 +62,7 @@ module.exports = {
     return res.view({
       //todo: 'Not implemented yet!',
       specs: spectators.join(', '),
+      board: defaultBoard,
     });
   },
 };
