473a6d71ab2634624c102a6d5c1105542968b70b
Eric Chu <eric@ericchu.net>
2015-03-06 14:33:28 -0500

    Added move piece request
    Changed notation to ICCF

--- assets/js/game/home.js
+++ assets/js/game/home.js
@@ -1,11 +1,40 @@
 $(function () {
   $('#board').on('click', '.square', function () {
-    var $piece = $(this).find('.piece');
-    var active = $piece.hasClass('active');
+    // A couple cases here
+    // 1. No pieces are active
+    //    a. User clicks on a piece
+    //       i. User owns the piece    
+    //      ii. User does not own the piece
+    //    b. User clicks on a square
+    // 2. A piece is active
+    //    a. User clicks on active piece
+    //    b. User clicks on inactive piece
+    //    c. User clicks on a square
 
-    $('.piece').removeClass('active');
-    if (!active) {
-      $(this).children().addClass('active');
+    var $activePiece = $('.piece.active');
+    var $piece = $(this).find('.piece');
+    
+    if ($activePiece.length) {
+      $activePiece.removeClass('active');
+      if (!$piece.hasClass('active')) {
+        // Request a move to target square
+        var start = getNotation($activePiece.parents('.square'));
+        var end = getNotation($(this));
+        io.socket.post('/move', {
+          start: start,
+          end: end,
+        });
+        console.log('Requesting ' + start + ' to ' + end);
+      }
+    } else {
+      if ($piece.length) {
+        // Handle all 1a cases
+        $piece.addClass('active');
+      }
     }
   })
-})
+});
+
+function getNotation($square) {
+  return $square.attr('id').substr(7);
+}
